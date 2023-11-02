550fdc767bc6153af248dccdab8a362e2ac152ab
DanielEnchev48 <neroenchev@gmail.com>
2023-11-02 22:50:10 +0200

    Update config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/dist/',
     build: {
+        chunkSizeWarningLimit: 1500,
         rollupOptions: {
             output: {
                 manualChunks(id) {
