4c7ecb5522c4fb57420a6582c555bc93b24a49b6
DanielEnchev48 <neroenchev@gmail.com>
2023-11-09 22:37:30 +0200

    Footer finished

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
                 <div className="row">
                     <div className="col-md-6 mt-md-0 mt-3 footerContent">
                         <h5 className="text-uppercase">Restaurant Website</h5>
-                        <p>Fully working responsive restaurant website with backend database, RESTful API requests, login system, shopping cart and more...</p>
+                        <p>Fully working responsive restaurant website written using React, Node and SASS, build with Vite, hosted on Github Pages (frontend only) with backend database, RESTful API requests,payment system, login system, shopping cart and more...</p>
                     </div>
 
                     <hr className="clearfix w-100 d-md-none pb-0" />
@@ -17,26 +17,25 @@ function Footer() {
                     <div className="col-md-3 mb-md-0 mb-3 techs">
                         <h5 className="text-uppercase">Technologies</h5>
                         <ul className="list-unstyled">
-                            <li>ReactJS</li>
-                            <li>ViteJS</li>
-                            <li>SASS</li>
-                            <li>Bootstrap</li>
+                            <li><a href="https://react.dev/">ReactJS</a></li>
+                            <li><a href="https://nodejs.org/en">NodeJS</a></li>
+                            <li><a href="https://sass-lang.com/documentation/">SASS</a></li>
+                            <li><a href="https://react-bootstrap.netlify.app/">Bootstrap</a></li>
                         </ul>
                     </div>
 
                     <div className="col-md-3 mb-md-0 mb-3 env ">
                         <h5 className="text-uppercase">Environment</h5>
                         <ul className="list-unstyled envList">
-                            <li><span>Github</span> - domain and code hosting</li>
-                            <li><span>MongoDB</span> - database</li>
-                            <li><span>ViteJS</span> - build system</li>
-                            <li><span>Jira</span> - tracking tool</li>
+                            <li><a href="https://github.com/">Github</a> - hosting and code hosting</li>
+                            <li><a href="https://www.mongodb.com/">MongoDB</a> - database</li>
+                            <li><a href="https://vitejs.dev/">ViteJS</a> - build system</li>
+                            <li><a href="https://www.atlassian.com/software/jira">Jira</a> - tracking tool</li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div className="footer-copyright text-center py-3">© 2023 Copyright:
-                <a> ElixirRestaurant</a>
+            <div className="footer-copyright text-center py-3">© 2023 Copyright: ElixirRestaurant
             </div>
         </div>
     </>
